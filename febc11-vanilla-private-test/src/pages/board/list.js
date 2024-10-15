79c9a271ac8c6d7c64d56c3064cecb05787c5a52
Jinsu <injnamek@gmail.com>
2024-10-15 16:30:10 +0900

    vite.config.js 파일 생성

--- febc11-vanilla-private-test/src/pages/board/list.js
+++ febc11-vanilla-private-test/src/pages/board/list.js
@@ -1,1 +1,22 @@
 import axios from "axios";
+
+// (1) API 서버에서 게시물 목록 조회
+async function getList() {
+  const response = await axios.get("https://11.fesp.shop/posts", {
+    params: { type: "test", page: 1, limit: 10 },
+  });
+  return response.data;
+}
+
+// (2) 조회한 게시물 목록을 화면에 출력
+async function renderList() {
+  const list = await getList();
+  const liLists = list.item.map((post) => {
+    return `
+    <li>
+      <h2>[${post._id}] ${post.title}</h2>
+      <span>조회수 : ${post.views}</span>
+      <span>날짜 : ${post.creat}
+    `;
+  });
+}
